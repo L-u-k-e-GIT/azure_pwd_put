3f2aaa7ab5c89d2efb4ac3cae8739fb8cf72c1b2
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-07-18 12:41:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#### Define again provider , required if Vault is on a different subscription
 terraform {
   required_providers {
 
@@ -16,7 +17,6 @@ resource "random_password" "vmpassword" {
 }
 
 resource "azurerm_key_vault_secret" "vmpassword" {
-
   count        = var.MD_RUN
   name         = "pwd-${var.MD_VM_NAME_PREFIX}${var.MD_PROJECT_NAME}-${var.MD_VM_NAME}-${var.MD_VM_ENV}"
   value        = random_password.vmpassword[0].result
@@ -31,18 +31,23 @@ output "vm_password" {
   sensitive = true
 }
 
-
+###############################VARIABLES
 
 variable "MD_VAULT_ID" {}
-variable "MD_VM_NAME" {}
-variable "MD_VM_NAME_PREFIX" {}
-variable "MD_PROJECT_NAME" {}
-variable "MD_SUBSCRIPTION_PREFIX" {}
 variable "MD_ALL_TAGS" {}
 variable "MD_ADMIN_USERNAME" {}
-variable "MD_VM_ENV" {}
+
 variable "MD_RUN" {
-  description = "ID of Vm Application"
+  description = "On/Off module. Useful to skip module on the root tf. 0 will not run, 1 will run the module"
   type        = number
   default     = "0"
 }
+
+
+###############################VARIABLES Naming Convention
+
+variable "MD_VM_NAME_PREFIX" {}
+variable "MD_PROJECT_NAME" {}
+variable "MD_SUBSCRIPTION_PREFIX" {}
+variable "MD_VM_ENV" {}
+variable "MD_VM_NAME" {}
